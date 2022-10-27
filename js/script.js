70eb377442b79b2fca6e50197794f36bee2f25b5
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-10-26 22:35:11 -0300

    pagina SPA

--- js/script.js
+++ js/script.js
@@ -1,8 +1,43 @@
 const botaoHome = document.querySelector('.botao--home');
+const botaoSobre = document.querySelector('.botao--sobre');
+const botaoAranhas = document.querySelector('.botao--aranhas');
+const botaoFilmes = document.querySelector('.botao--filmes');
 
+const paginaHome = document.querySelector('.conteudo--home');
+const paginaSobre = document.querySelector('.conteudo--sobre');
 const paginaAranhas = document.querySelector('.conteudo--aranhas');
-
+const paginaFilmes = document.querySelector('.conteudo--filmes');
 
 botaoHome.addEventListener('click', function () {
-    paginaAranhas.classList.add('inativa');
+    paginaHome.classList.remove('pagina--inativa');
+    
+    paginaSobre.classList.add('pagina--inativa');
+    paginaAranhas.classList.add('pagina--inativa');
+    paginaFilmes.classList.add('pagina--inativa');
 });
+
+botaoSobre.addEventListener('click', function () {
+    paginaSobre.classList.remove('pagina--inativa');
+
+    paginaHome.classList.add('pagina--inativa');
+    paginaAranhas.classList.add('pagina--inativa');
+    paginaFilmes.classList.add('pagina--inativa');
+});
+
+botaoAranhas.addEventListener('click', function () {
+    paginaAranhas.classList.remove('pagina--inativa');
+    
+    paginaSobre.classList.add('pagina--inativa');
+    paginaHome.classList.add('pagina--inativa');
+    paginaFilmes.classList.add('pagina--inativa');
+});
+
+botaoFilmes.addEventListener('click', function () {
+    paginaFilmes.classList.remove('pagina--inativa');
+
+    paginaHome.classList.add('pagina--inativa');
+    paginaAranhas.classList.add('pagina--inativa');
+    paginaSobre.classList.add('pagina--inativa');
+});
+
+
