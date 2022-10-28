9d9f6937ef8495bfa5862d2f69865c0daa4560aa
Bruno S. Machado <bruno.sabbagmachado@outlook.com>
2022-10-27 23:38:02 -0300

    refatorando código e arrumando botoes

--- js/script.js
+++ js/script.js
@@ -10,34 +10,50 @@ const paginaFilmes = document.querySelector('.conteudo--filmes');
 
 botaoHome.addEventListener('click', function () {
     paginaHome.classList.remove('pagina--inativa');
-    
     paginaSobre.classList.add('pagina--inativa');
     paginaAranhas.classList.add('pagina--inativa');
     paginaFilmes.classList.add('pagina--inativa');
+
+    botaoHome.classList.add('botao--ativo');
+    botaoSobre.classList.remove('botao--ativo');
+    botaoAranhas.classList.remove('botao--ativo');
+    botaoFilmes.classList.remove('botao--ativo');
 });
 
 botaoSobre.addEventListener('click', function () {
     paginaSobre.classList.remove('pagina--inativa');
-
     paginaHome.classList.add('pagina--inativa');
     paginaAranhas.classList.add('pagina--inativa');
     paginaFilmes.classList.add('pagina--inativa');
+
+    botaoHome.classList.remove('botao--ativo');
+    botaoSobre.classList.add('botao--ativo');
+    botaoAranhas.classList.remove('botao--ativo');
+    botaoFilmes.classList.remove('botao--ativo');
 });
 
 botaoAranhas.addEventListener('click', function () {
     paginaAranhas.classList.remove('pagina--inativa');
-    
     paginaSobre.classList.add('pagina--inativa');
     paginaHome.classList.add('pagina--inativa');
     paginaFilmes.classList.add('pagina--inativa');
+
+    botaoHome.classList.remove('botao--ativo');
+    botaoSobre.classList.remove('botao--ativo');
+    botaoAranhas.classList.add('botao--ativo');
+    botaoFilmes.classList.remove('botao--ativo');
 });
 
 botaoFilmes.addEventListener('click', function () {
     paginaFilmes.classList.remove('pagina--inativa');
-
     paginaHome.classList.add('pagina--inativa');
     paginaAranhas.classList.add('pagina--inativa');
     paginaSobre.classList.add('pagina--inativa');
+
+    botaoHome.classList.remove('botao--ativo');
+    botaoSobre.classList.remove('botao--ativo');
+    botaoAranhas.classList.remove('botao--ativo');
+    botaoFilmes.classList.add('botao--ativo');
 });
 
 
